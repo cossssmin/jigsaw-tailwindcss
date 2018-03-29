a21fedc7aac3a24d59477575dd04311039820097
Cosmin Popovici <hellocosmin@gmail.com>
2018-03-29 12:43:43 +0300

    Initial commit.

---
+++ source/index.blade.php
@@ -0,0 +1,10 @@
+@extends('_layouts.master')
+
+@section('body')
+
+<div class="content h-screen flex flex-col items-center justify-center bg-grey-lighter">
+    <h1 class="font-light">Jigsaw + Tailwind CSS</h1>
+    <p class="text-grey-dark mt-4">A starter kit for using <a href="https://tailwindcss.com/">Tailwind CSS</a> with <a href="http://jigsaw.tighten.co/">Jigsaw</a></p>
+</div>
+
+@endsection
---
+++ webpack.mix.js
@@ -0,0 +1,29 @@
+let mix = require('laravel-mix');
+let tailwind = require('tailwindcss');
+let build = require('./tasks/build.js');
+require('laravel-mix-purgecss');
+
+mix.disableSuccessNotifications();
+mix.setPublicPath('source/assets/');
+mix.webpackConfig({
+    plugins: [
+        build.jigsaw,
+        build.browserSync(),
+        build.watch(['source/**/*.md', 'source/**/*.php', 'source/**/*.scss']),
+    ]
+});
+
+mix.js('source/_assets/js/main.js', 'js')
+    .sass('source/_assets/sass/main.scss', 'css/main.css')
+    .options({
+        processCssUrls: false,
+        postCss: [
+            tailwind('tailwind.js'),
+        ]
+    })
+    .purgeCss({
+        globs: [
+            path.join(__dirname, "source/**/*.blade.php"),
+        ],
+    })
+    .version();
