583975eb9cf36756d2e2c282fae2cfd2041f634c
Cosmin Popovici <hellocosmin@gmail.com>
2018-09-22 23:56:52 +0300

    Prevent Webpack from watching changes in _tmp directories.
    + reindent some js(on) files.

--- tasks/build.js
+++ tasks/build.js
@@ -12,35 +12,35 @@ let env = argv.e || argv.env || 'local';
 let port = argv.p || argv.port || 3000;
 
 module.exports = {
-    jigsaw: new AfterWebpack(() => {
-        command.get(bin.path() + ' build ' + env, (error, stdout, stderr) => {
-            console.log(error ? stderr : stdout);
+  jigsaw: new AfterWebpack(() => {
+    command.get(bin.path() + ' build ' + env, (error, stdout, stderr) => {
+      console.log(error ? stderr : stdout);
 
-            if (browserSyncInstance) {
-                browserSyncInstance.reload();
-            }
-        });
-    }),
+      if (browserSyncInstance) {
+        browserSyncInstance.reload();
+      }
+    });
+  }),
 
-    watch: function(paths) {
-        return new Watch({
-            options: { ignoreInitial: true },
-            paths: paths,
-        })
+  watch: function(paths) {
+    return new Watch({
+      options: { ignoreInitial: true },
+      paths: paths,
+    })
+  },
+
+  browserSync: function(proxy) {
+    return new BrowserSyncPlugin({
+      notify: false,
+      port: port,
+      proxy: proxy,
+      server: proxy ? null : { baseDir: 'build_' + env + '/' },
     },
-
-    browserSync: function(proxy) {
-        return new BrowserSyncPlugin({
-            notify: false,
-            port: port,
-            proxy: proxy,
-            server: proxy ? null : { baseDir: 'build_' + env + '/' },
-        },
-        {
-            reload: false,
-            callback: function() {
-                browserSyncInstance = BrowserSync.get('bs-webpack-plugin');
-            },
-        })
-    },
+    {
+      reload: false,
+      callback: function() {
+        browserSyncInstance = BrowserSync.get('bs-webpack-plugin');
+      },
+    })
+  },
 };
--- webpack.mix.js
+++ webpack.mix.js
@@ -6,22 +6,22 @@ require('laravel-mix-purgecss');
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/');
 mix.webpackConfig({
-    plugins: [
-        build.jigsaw,
-        build.browserSync(),
-        build.watch(['source/**/*.md', 'source/**/*.php', 'source/**/*.scss']),
-    ]
+  plugins: [
+    build.jigsaw,
+    build.browserSync(),
+    build.watch(['source/**/*.md', 'source/**/*.php', 'source/**/*.scss', '!source/**/_tmp/*']),
+  ]
 });
 
 mix.js('source/_assets/js/main.js', 'js')
-    .sass('source/_assets/sass/main.scss', 'css/main.css')
-    .options({
-        processCssUrls: false,
-        postCss: [
-            tailwind('tailwind.js'),
-        ]
-    })
-    .purgeCss({
-        folders: ['source'],
-    })
-    .version();
+  .sass('source/_assets/sass/main.scss', 'css/main.css')
+  .options({
+    processCssUrls: false,
+    postCss: [
+      tailwind('tailwind.js'),
+    ]
+  })
+  .purgeCss({
+    folders: ['source'],
+  })
+  .version();
