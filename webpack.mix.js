4a7469fd203e9de3a379b42eb8ab9b44ff2c9ac6
Cosmin Popovici <hellocosmin@gmail.com>
2018-11-15 15:05:53 +0200

    Fix CSS files extension for watch command.

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,7 +9,7 @@ mix.webpackConfig({
   plugins: [
     build.jigsaw,
     build.browserSync(),
-    build.watch(['source/**/*.md', 'source/**/*.php', 'source/**/*.scss', '!source/**/_tmp/*']),
+    build.watch(['source/**/*.md', 'source/**/*.php', 'source/**/*.css', '!source/**/_tmp/*']),
   ]
 });
 
