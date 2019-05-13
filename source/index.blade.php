6646f2958f5d462f8d023e2d640e213598d0fbcf
Cosmin Popovici <hellocosmin@gmail.com>
2019-05-13 20:04:48 +0300

    Update dependencies.
    Most notably, Tailwind 1.0 and latest Jigsaw.

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 
-<div class="content h-screen flex flex-col items-center justify-center bg-grey-lighter">
-    <h1 class="font-light">Jigsaw + Tailwind CSS</h1>
-    <p class="text-grey-dark mt-4">A starter kit for using <a href="https://tailwindcss.com/">Tailwind CSS</a> with <a href="http://jigsaw.tighten.co/">Jigsaw</a></p>
-</div>
+<main class="main">
+    <h1 class="font-light text-3xl text-black">Jigsaw + Tailwind CSS</h1>
+    <p class="text-gray-600 mt-4">A starter kit for using <a href="https://tailwindcss.com/">Tailwind CSS</a> with <a href="http://jigsaw.tighten.co/">Jigsaw</a></p>
+</main>
 
 @endsection
